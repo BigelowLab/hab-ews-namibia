de1381ba1dbd57e2f42ca092e8df4486efd48066
jevanilla <jevanilla@bigelow.org>
2024-10-18 13:57:29 -0700

    Adding data analysis example and setup script

---
+++ setup.R
@@ -0,0 +1,14 @@
+# Setup file for the workshop: "Introduction to Developing Interactive Shiny Applications in R"
+
+installed = rownames(installed.packages())
+
+cran_packages <- c("dplyr", "ggplot2", "readxl")
+
+ix = (cran_packages %in% installed)
+for (package in cran_packages[!ix]) {
+  install.packages(package)
+}
+
+suppressPackageStartupMessages({
+  for (package in cran_packages) library(package, character.only = TRUE)
+})
