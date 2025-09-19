9fb16045b4d95a2db155d28dabb75d0387c15b34
jevanilla <jevanilla@bigelow.org>
2025-09-19 12:32:50 -0700

    Updated setup script with reader functions; updated grower report on 18 September

--- setup.R
+++ setup.R
@@ -1,14 +1,37 @@
-# Setup file for the workshop: "Introduction to Developing Interactive Shiny Applications in R"
+## Namibia HAB Data Setup Script
 
-installed = rownames(installed.packages())
+# Check for installed packages
+installed = dplyr::as_tibble(installed.packages())
 
-cran_packages <- c("dplyr", "ggplot2", "readxl", "knitr", "rmarkdown", "leaflet", "sf", "rnaturalearth")
+# Required packages
+packages = list(
+  cran = c("readr", "dplyr", "ggplot2", "tidyr", "readxl", "forcats", "knitr", "kableExtra")
+)
 
-ix = (cran_packages %in% installed)
-for (package in cran_packages[!ix]) {
-  install.packages(package)
+# If a required package isn't installed, install it; load the package
+for (package in packages$cran){
+  if (!package %in% installed$Package) {
+    cat("installing", package, "from CRAN\n")
+    install.packages(package)
+  }
+  suppressPackageStartupMessages(library(package, character.only = TRUE))
 }
 
-suppressPackageStartupMessages({
-  for (package in cran_packages) library(package, character.only = TRUE)
-})
+# Phytoplankton monitoring data reader function
+read_phyto_data <- function(file = "data/Phytoplankton Total Cell_L_2018_2024.xlsx") {
+  read_excel(file) |>
+    mutate(`Date Collected` = as.Date(`Date Collected`),
+           month = format(`Date Collected`, format="%m"),
+           week = format(`Date Collected`, format="%U"),
+           year = format(`Date Collected`, format="%Y"),
+           date = as.Date(`Date Collected`, format="%Y-%m-%d")) |>
+    arrange(`Date Collected`) |>
+    distinct() |>
+    filter(!is.na(`Cells/L`))
+}
+
+# Alert threshold lookup reader
+read_threshold <- function(file = "data/alert_thresholds.xlsx") {
+  read_excel(file)
+}
+
