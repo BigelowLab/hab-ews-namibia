7211bd117ef2e593c2ece3d889499d407035d03d
jevanilla <jevanilla@bigelow.org>
2025-10-02 11:12:47 -0400

    Adding updated setup script and grower report from 2 october

--- setup.R
+++ setup.R
@@ -5,7 +5,7 @@ installed = dplyr::as_tibble(installed.packages())
 
 # Required packages
 packages = list(
-  cran = c("readr", "dplyr", "ggplot2", "tidyr", "readxl", "forcats", "knitr", "kableExtra")
+  cran = c("readr", "dplyr", "ggplot2", "tidyr", "readxl", "forcats", "knitr", "kableExtra", "here")
 )
 
 # If a required package isn't installed, install it; load the package
@@ -17,8 +17,12 @@ for (package in packages$cran){
   suppressPackageStartupMessages(library(package, character.only = TRUE))
 }
 
+# project root
+here::i_am("setup.R")
+
 # Phytoplankton monitoring data reader function
-read_phyto_data <- function(file = "data/Phytoplankton Total Cell_L_2018_2024.xlsx") {
+read_phyto_data <- function(#file = "data/Phytoplankton Total Cell_L_2018_2024.xlsx",
+                            file = here::here("data", "Phytoplankton Total Cell_L_2018_2024.xlsx")) {
   read_excel(file) |>
     mutate(`Date Collected` = as.Date(`Date Collected`),
            month = format(`Date Collected`, format="%m"),
@@ -30,8 +34,19 @@ read_phyto_data <- function(file = "data/Phytoplankton Total Cell_L_2018_2024.xl
     filter(!is.na(`Cells/L`))
 }
 
+read_env_data <- function(file = here::here("data", "MultiMeter_Spectro-Data.xlsx")) {
+  read_excel(file) |>
+    mutate(date = as.Date(`Sampling date`, format="%Y-%m-%d")) |>
+    select(-`Instrument model`)
+}
+
+read_nutrient_data <- function(file = here::here("data", "MultiMeter_Spectro-Data.xlsx")) {
+  read_excel(file, sheet=2, range = cell_cols("A:E"))
+}
+
 # Alert threshold lookup reader
-read_threshold <- function(file = "data/alert_thresholds.xlsx") {
+read_threshold <- function(#file = "data/alert_thresholds.xlsx",
+                           file = here::here("data", "alert_thresholds.xlsx")) {
   read_excel(file)
 }
 
