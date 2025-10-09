b161880e945ed4a767e8316e8d707fe5711f4628
jevanilla <jevanilla@bigelow.org>
2025-10-09 16:26:14 -0700

    Adding biotoxin reader function; updated nssp report to use setup script; added dsp plots

--- setup.R
+++ setup.R
@@ -34,6 +34,29 @@ read_phyto_data <- function(#file = "data/Phytoplankton Total Cell_L_2018_2024.x
     filter(!is.na(`Cells/L`))
 }
 
+read_toxin_data <- function(file = here::here("data", "tbe_Biotoxin Tests_2019_2024.xlsx")) {
+  r <- read_excel(file) |>
+    mutate(`Sampling Date` = as.Date(`Sampling Date`),
+           week = format(`Sampling Date`, format="%U"),
+           `Total Okadaic Acid value` = as.numeric(case_when(`Total Okadaic Acid` == "Detected" ~ `Total Okadaic Acid value`,
+                                                             `Total Okadaic Acid` == "Not detected" ~ "0",
+                                                             `Total Okadaic Acid` == "under detection limit" ~ "0",
+                                                             `Total Okadaic Acid` == "Not tested" ~ NA,
+                                                             grepl(",", `Total Okadaic Acid value`) ~ gsub(",", ".", `Total Okadaic Acid value`))),
+            `Total DTX-1 value` = as.numeric(case_when(`Total DTX-1` == "Detected" ~ `Total DTX-1 value`,
+                                                       `Total DTX-1` == "Not Detected" ~ "0",
+                                                       `Total DTX-1` == "under detection limit" ~ "0",
+                                                       `Total DTX-1` == "Not tested" ~ NA,
+                                                       grepl(",", `Total DTX-1 value`) ~ gsub(",", ".", `Total DTX-1 value`))),
+           `Yessotoxin value` = as.numeric(case_when(Yessotoxin == "Detected" ~ `Yessotoxin value`,
+                                                     Yessotoxin == "Not detected" ~ "0",
+                                                     Yessotoxin == "under detection limit" ~ "0",
+                                                     Yessotoxin == "Not tested" ~ NA)),
+           okadaic_acid = `Total Okadaic Acid value` + `Total DTX-1 value`)
+  
+  return(r)
+}
+
 read_env_data <- function(file = here::here("data", "MultiMeter_Spectro-Data.xlsx")) {
   read_excel(file) |>
     mutate(date = as.Date(`Sampling date`, format="%Y-%m-%d")) |>
@@ -49,4 +72,6 @@ read_threshold <- function(#file = "data/alert_thresholds.xlsx",
                            file = here::here("data", "alert_thresholds.xlsx")) {
   read_excel(file)
 }
+
+
 
